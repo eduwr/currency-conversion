1265b68d9618bacc8c202f226e2b5309d7044522
eduwr <eduwric@gmail.com>
2021-04-10 10:02:53 -0300

    feat: getCurrency should be called twice

--- src/exchange/exchange.service.ts
+++ src/exchange/exchange.service.ts
@@ -1,10 +1,22 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Inject } from '@nestjs/common';
+
+export class CurrenciesService {
+  async getCurrency(currency: string): Promise<any> {
+    // mock
+  }
+}
 
 @Injectable()
 export class ExchangeService {
-  async convertAmount({ from, to, amount }): Promise<any> {
+  constructor(private currenciesService: CurrenciesService) {}
+
+  convertAmount = async ({ from, to, amount }): Promise<any> => {
     if (!from || !to || !amount) {
       throw new BadRequestException();
     }
-  }
+
+    const currencyFrom = this.currenciesService.getCurrency(from);
+
+    const currencyTo = this.currenciesService.getCurrency(to);
+  };
 }
